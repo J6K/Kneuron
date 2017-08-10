fdb74a59e3cf34b98843c9cbb70ce17ef5108e3d
J6K <jmkang06@gmail.com>
2017-08-09 17:34:38 -0700

    [Fix] - cleaned up code and ensured functionality

--- server/api/admin.js
+++ server/api/admin.js
@@ -3,22 +3,18 @@ const bcrypt = require('bcryptjs');
 const db = require('../db/models');
 const hasher = require('./util').hasher;
 
-
 const fetchAdmin = async (req, res) => {
   try {
     const user = await db.User.findOne({ where: { email: req.params.email, userType: 2 } });
-    const data = await bcrypt.compare(req.params.creds, user.password);
-    if (data) {
-      res.status(200).send({ user, id_token: hasher(req.params.email) });
-    } else {
+    const verified = await bcrypt.compare(req.params.creds, user.password);
+    verified ?
+      res.status(200).send({ user, id_token: hasher(req.params.email) }) :
       res.status(404).send('Credentials incorrect');
-    }
   } catch (error) {
     console.log('User Does Not Exist');
     res.status(404).send(error);
   }
 };
-
 
 router.get('/:email/:creds', fetchAdmin);
 
