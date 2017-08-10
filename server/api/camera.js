50e6668fb6bc2c5b4f42ee321e0daa5907253c36
J6K <jmkang06@gmail.com>
2017-08-09 17:35:06 -0700

    [Fix] - cleaned up code

--- server/api/camera.js
+++ server/api/camera.js
@@ -3,19 +3,27 @@ const axios = require('axios');
 
 const app_id = '59193b0b';
 const app_key = '625d66480c5897855db7b295808b465b';
+const config = {
+  headers: {
+    'Content-Type': 'application/json',
+    'app_id': app_id,
+    'app_key': app_key,
+  },
+};
 
-router.post('/', (req, res, next) => {
-  const config = {
-    headers: {
-      'Content-Type': 'application/json',
-      'app_id': app_id,
-      'app_key': app_key,
-    },
-  };
-  axios.post('https://api.kairos.com/enroll', { image: req.body.image, subject_id: req.body.subject_id, gallery_name: req.body.gallery_name }, config)
-  .then(({ data }) => {
-    res.status(201).send(data);
-  });
-});
+const sendPictureToKairos = async (req, res) => {
+  try {
+    const kairosResult = await axios.post('https://api.kairos.com/enroll', {
+      image: req.body.image,
+      subject_id: req.body.subject_id,
+      gallery_name: req.body.gallery_name,
+    }, config);
+    res.status(201).send(kairosResult);
+  } catch (error) {
+    res.status(error.statusCode).send(error);
+  }
+};
+
+router.post('/', sendPictureToKairos);
 
 module.exports = router;
--- server/api/facialVerify.js
+++ server/api/facialVerify.js
@@ -1,30 +1,30 @@
 const router = require('express').Router();
 const axios = require('axios');
-const db = require('../db/models');
 
 const app_id = '59193b0b';
 const app_key = '625d66480c5897855db7b295808b465b';
+const config = {
+  headers: {
+    'Content-Type': 'application/json',
+    'app_id': app_id,
+    'app_key': app_key,
+  },
+};
 
-router.post('/', (req, res, next) => {
-  console.log('helllooo from camera server boooiiii');
-  const config = {
-    headers: {
-      'Content-Type': 'application/json',
-      'app_id': app_id,
-      'app_key': app_key,
-    },
-  };
-  console.log('this is the gallery_name ', req.body.gallery_name)
-  console.log('this is the subject id ', req.body.subject_id)
-  axios.post("https://api.kairos.com/verify", { image: req.body.image, subject_id: req.body.subject_id, gallery_name: req.body.gallery_name }, config)
-  .then(data => {
-    console.log("this is the data from camera api line 10", data.data)
-    res.status(201).send(data.data)
-  })
-  .catch((err) => {
-    console.log('this is err in facial verify', err);
-  });
+const verifyFace = async (req, res) => {
+  try {
+    const result = await axios.post('https://api.kairos.com/verify', {
+      image: req.body.image,
+      subject_id: req.body.subject_id,
+      gallery_name: req.body.gallery_name,
+    }, config);
+    result ? res.status(201).send(result) : res.status(404).send('Result not found');
+  } catch (error) {
+    console.log('Error in facial verify', error);
+    res.status(500).send(error);
+  }
+};
 
-})
+router.post('/', verifyFace);
 
 module.exports = router;
